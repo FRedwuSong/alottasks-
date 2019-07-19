3bb369c4f79a259af5ddc3d81e9b6a5b9033c4d7
wusunghan   <diadora1166@gmail.com>
2019-07-19 22:06:36 +0800

    add feature test of update task

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :task do
+    title { "Title" }
+    content  { "Content " }
+  end
+end
--- spec/feature/tasks_spec.rb
+++ spec/feature/tasks_spec.rb
@@ -10,4 +10,14 @@ RSpec.feature "Task CRUD", :type => :feature do
 
     expect(page).to have_text("task create succefully")
   end
+  scenario "User updates a task" do
+    @task = FactoryBot.create(:task)
+    visit "/tasks/#{@task.id}/edit"
+
+    fill_in "task_title", :with => "update_title"
+    fill_in "task_content", :with => "update_content"
+    click_button "Update Task"
+
+    expect(page).to have_text("Success Edit")
+  end
 end
