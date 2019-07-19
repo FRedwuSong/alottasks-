ab8bebb5f3f97e6050fb7555b8d9dc797f577b42
wusunghan   <diadora1166@gmail.com>
2019-07-19 23:04:25 +0800

    Set Time Zone to "Taipei"

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Alottasks
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = "zh-TW"
-    
+    Time.zone = "Taipei" 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
