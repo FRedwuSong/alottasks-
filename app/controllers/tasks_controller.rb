446d2ef5ddebb46d2f1a38c94855e3eb146b0ce7
wusunghan   <diadora1166@gmail.com>
2019-07-19 14:58:32 +0800

    fix CRUD task_controller issue

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,6 +23,7 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task = Task.find(params[:id])
     if @task.update(task_params)
       redirect_to tasks_path, notice: "Success Edit"
     else
